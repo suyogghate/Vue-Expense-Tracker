5ffcf20feb3be3c79ecfe73cc22531b305836fe6
suyogghate <suyogghate96@gmail.com>
2025-04-04 17:41:54 +0530

    Initialised the expense tracker project. Added addTransaction, balance, header, incomeExpense and transactionList component.

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <Header />
+  <div class="container">
+    <Balance :total="+total"/>
+    <IncomeExpenses :income="+income" :expenses="+expenses"/>
+    <TransactionList :transactions="transactions"/>
+    <AddTransaction />
+  </div>
+</template>
+
+<script setup>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+import { ref, computed } from 'vue';
+
+const transactions = ref([
+    { id: 1, text: 'Flower', amount: -19.44 },
+    { id: 2, text: 'Salary', amount: 299.67 },
+    { id: 3, text: 'Book', amount: -10 },
+    { id: 4, text: 'Camera', amount: 150 },
+]);
+
+// Get total
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0)
+  .toFixed(2);
+});
+
+// Get Income
+const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+  }, 0)
+  .toFixed(2);
+});
+
+// Get expenses
+const expenses = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+  }, 0)
+  .toFixed(2);
+});
+</script>
+
+
+<style scoped>
+
+</style>
